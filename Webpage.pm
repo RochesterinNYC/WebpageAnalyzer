11fd30c17f32719e4dbae477fecddcc08e7a52f7
James Wen <jrw2175@columbia.edu>
2013-12-12 02:49:23 -0500

    Parsing of images and links implemented

--- Webpage.pm
+++ Webpage.pm
@@ -28,32 +28,54 @@ sub analyzeElements {
   my($self) = @_;
   my @lines = split(/\n/, $self->{code});
   foreach(@lines){
-    if($_ =~ m/<([\w]+[\d]*)/g){
-      print $_ . "\n";
+    if($_ =~ m/<([\w]+[\d]*)([\d\w\s].*?)?>/g){
       $self->{elements}{$1} += 1;
       if($1 eq 'a'){
-        $self->processImage($2);
+        print "test\n";
+        $self->processLink($2);
       }
       elsif($1 eq 'img'){
-        $self->processLink($2);
+        print "img\n";
+
+        $self->processImage($2);
       }
     }
   }
   foreach my $key (keys %{$self->{elements}}){
     print $key . ": " . $self->{elements}{$key} . " \n";
   }
+  print "       Links       \n";
+  foreach(@{$self->{links}}){
+    print $_ . " \n";
+  }
+  print "       Images       \n";
+  foreach(@{$self->{images}}){
+    print $_ . " \n";
+  }
 }
 
 sub processLink {
   my($self, $link) = @_;
-  if($link =~ m/href[\s]*=[\s]*"[\s]([\w]*)[\s]"/g){
-    push(@{$self->{links}}, $1);
+  print $link . "\n";
+  if($link =~ m/href[\s]*=[\s]*"([\s]*[\/\d\w:?&#%=~+!_.-]*[\s]*)??"/g){
+    $url = $1;
+    if(!((substr($url, 0, 5) eq 'http:') || (substr($url, 0, 6) eq 'https:'))){
+      $url = $self->{url} . '/' . $url;
+    }
+    push(@{$self->{links}}, $url);
   }
 }
 sub processImage {
   my($self, $image) = @_;
-  if($image =~ m/src[\s]*=[\s]*"[\s]([\w]*)[\s]"/g){
-    push(@{$self->{images}}, $1);
+  print $image . "\n";
+
+  if($image =~ m/src[\s]*=[\s]*"([\s]*[\/\d\w:?&#%=~+!_.-]*[\s]*)??"/g){
+
+    $url = $1;
+    if(!((substr($url, 0, 5) eq 'http:') || (substr($url, 0, 6) eq 'https:'))){
+      $url = $self->{url} . '/' . $url;
+    }
+    push(@{$self->{images}}, $url);
   }
 }
 
